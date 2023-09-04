03d0072d5a1754c8b5e5ada8d88a89ec8a534f01
Sebastian <spartan89632@hotmail.com>
2023-09-04 12:42:36 -0500

    adding map API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,30 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+ionViewDidEnter() {
+  setTimeout(async () => {
+  await this.createMap();
+    }, 500);
+ }
+
+@ViewChild('map') public mapEl: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
+
+  private async createMap(): Promise<void>{
+    this.newMap = await GoogleMap.create({
+      id: 'my-cool-map',
+      element: this.mapEl.nativeElement,
+      apiKey: environment.keys.googleMaps,
+      config: {
+        center: {
+          lat: 4.730232,
+          lng: -74.065981
+        },
+        zoom: 10,
+      },
+    });
+  }
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBHE_iD9_KJ9BJClYZhy7YmCfhnxfgeueI",
+    authDomain: "esp8266-gps-eee74.firebaseapp.com",
+    databaseURL: "https://esp8266-gps-eee74-default-rtdb.firebaseio.com/",
+    projectId: "esp8266-gps-eee74",
+    storageBucket: "esp8266-gps-eee74.appspot.com",
+    messagingSenderId: "203726153205",
+    appId: "1:203726153205:web:df90e830579d2e48deecbc"
+  },
+  keys: {
+     googleMaps: 'AIzaSyBPsDYHOlTPse4dtzDI58PjZvMl9eWLwFM'
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
